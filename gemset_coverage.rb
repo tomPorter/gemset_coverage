36b75b726d10a36362d2e178ec6315b1c3b74d25
Tom Porter <tom.porter@solutionset.com>
2011-07-11 17:17:14 -0400

    Close to working as I want

--- gemset_coverage.rb
+++ gemset_coverage.rb
@@ -7,7 +7,7 @@ class GemCoverage
   end
 
   def add_gemset_version(gemset,*versions)
-    @gemset_versions[gemset] < versions 
+    @gemset_versions[gemset] = versions 
   end
 
   def to_s
@@ -15,29 +15,62 @@ class GemCoverage
   end
 end
 
-def print_gems(ruby_version,gemset = '')
-  if gemset == ''
-    p  "#{ruby_version} default"
-    RVM.environment("#{ruby_version}").rvm :use,  "#{ruby_version}"
-  else
-    p  "#{ruby_version}@#{gemset}"
-    RVM.environment("#{ruby_version}@#{gemset}").rvm :use,  "#{ruby_version}@#{gemset}"
+class GemEntry
+  attr_accessor :name, :versions
+  def initialize()
+    @name = ''
+    @versions = []
   end
-  current_gems = RVM.environment("#{ruby_version}@#{gemset}").run_command('gem list')[1].split(/\n/)
-  current_gems.each do |g| 
-    p "   #{g}"
-     
+
+  def split_gem_entry(line)
+    @name, version_string = line.split(/\s/,2)
+    @versions = version_string[1..-2].split(',')
+	end
+end
+
+def gem_list(ruby_version,gemset = '')
+  ## ToDo: currently returns list of strings in "gem_name (ver1, ver2, ver3)" format
+  ## ToDo: Need to break out into gem names and list of versions, then flesh out
+  ## ToDo: classes.
+  if gemset == ''
+    rvm_version = ruby_version
+  else
+    rvm_version = ruby_version + '@' + gemset
+  end
+  current_gems = RVM.environment("#{rvm_version}").run_command('gem list')[1].split(/\n/)
+  current_gems
+end
+
+def update_gem_coverage_hash(gc_hash,gem_entry,gemset)
+  if gc_hash.has_key? gem_entry.name
+    gc_hash[gem_entry.name].add_gemset_version(gemset,gem_entry.versions)
+  else
+    gc_hash[gem_entry.name] = GemCoverage.new(gem_entry.name)
+    gc_hash[gem_entry.name].add_gemset_version(gemset,gem_entry.versions)
   end
 end
 
 $LOAD_PATH << '~/.rvm/lib'
 require 'rvm'
+gemset_coverage_hash = {}
 current_ruby = '1.9.2'
-parent_env = RVM.environment(current_ruby)
-print_gems(current_ruby)
-current_gemsets = parent_env.gemset_list[1..999]
-gem_list = {}
-current_gemsets.each do |gemset|
-  print_gems(current_ruby,gemset)
+p current_ruby
+#gem_list(current_ruby).each { |g| p "  #{g}" }
+gem_list(current_ruby).each do |g| 
+  gem_entry = GemEntry.new()
+  gem_entry.split_gem_entry(g)
+  update_gem_coverage_hash(gemset_coverage_hash,gem_entry,'default')
 end
+parent_env = RVM.environment(current_ruby)
+current_gemsets = parent_env.gemset_list[1..999]
+current_gemsets.each do |gemset|
+  p "#{current_ruby}@#{gemset}"
+  gem_list(current_ruby,gemset).each do |g| 
+    gem_entry = GemEntry.new()
+    gem_entry.split_gem_entry(g)
+    update_gem_coverage_hash(gemset_coverage_hash,gem_entry,gemset)
+  end 
+end
+#p gemset_coverage_hash
+p gemset_coverage_hash['activemodel']
 
