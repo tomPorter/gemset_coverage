e0b9d131ef855a3bf4491fcde794b18fe0e412a8
tomPorter <thomas.porter@acm.org>
2011-07-11 11:20:35 -0400

    Added Default gems to display

--- gemset_coverage.rb
+++ gemset_coverage.rb
@@ -15,19 +15,29 @@ class GemCoverage
   end
 end
 
-$LOAD_PATH << '~/.rvm/lib'
-require 'rvm'
-current_ruby = '1.9.2'
-parent_env = RVM.environment(current_ruby)
-current_gemsets = parent_env.gemset_list[1..999]
-gem_list = {}
-current_gemsets.each do |gemset|
-  p  "#{current_ruby}@#{gemset}"
-  RVM.environment("#{current_ruby}@#{gemset}").rvm :use,  "#{current_ruby}@#{gemset}"
-  current_gems = RVM.environment("#{current_ruby}@#{gemset}").run_command('gem list')[1].split(/\n/)
+def print_gems(ruby_version,gemset = '')
+  if gemset == ''
+    p  "#{ruby_version} default"
+    RVM.environment("#{ruby_version}").rvm :use,  "#{ruby_version}"
+  else
+    p  "#{ruby_version}@#{gemset}"
+    RVM.environment("#{ruby_version}@#{gemset}").rvm :use,  "#{ruby_version}@#{gemset}"
+  end
+  current_gems = RVM.environment("#{ruby_version}@#{gemset}").run_command('gem list')[1].split(/\n/)
   current_gems.each do |g| 
     p "   #{g}"
      
   end
+end
+
+$LOAD_PATH << '~/.rvm/lib'
+require 'rvm'
+current_ruby = '1.9.2'
+parent_env = RVM.environment(current_ruby)
+print_gems(current_ruby)
+current_gemsets = parent_env.gemset_list[1..999]
+gem_list = {}
+current_gemsets.each do |gemset|
+  print_gems(current_ruby,gemset)
 end
 
