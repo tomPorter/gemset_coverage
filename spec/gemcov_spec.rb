58aaccb73a9aae6fdc4a102a832b37c1e7653466
Tom Porter <tom.porter@solutionset.com>
2011-07-27 16:52:24 -0400

    Added more tests on Gem Coverage Entry

--- spec/gemcov_spec.rb
+++ spec/gemcov_spec.rb
@@ -23,12 +23,41 @@ describe "Gem Coverage Entry" do
     gce.gemset_versions.include?(gemset_name).should == true
     gce.gemset_versions[gemset_name].should == versions
   end
-  it "should identfy when a gem exists in all gemsets but the global one" do 
+  it ".all_gemsets_but_global? should return 'true' when a gem exists in all gemsets, the default ruby instance, but not in 'global'" do 
     gce = GemCov::GemCoverageEntry.new('aGem')
     versions = ['1.0.0']
-    global_name = 'global'
-    all_gemsets = ['global','default']
-    gce.add_gemset_versions(global_name,versions)
+    all_gemsets = ['another_gemset','my_gemset','default']
+    all_gemsets.collect {|gs| gce.add_gemset_versions(gs,versions) }
+    gce.in_all_gemsets_but_global?(all_gemsets).should == true
+  end
+  it ".all_gemsets_but_global? should return 'true' when a gem exists in all gemsets but not in 'global'" do 
+    gce = GemCov::GemCoverageEntry.new('aGem')
+    versions = ['1.0.0']
+    all_gemsets = ['another_gemset','my_gemset']
+    all_gemsets.collect {|gs| gce.add_gemset_versions(gs,versions) }
+    gce.in_all_gemsets_but_global?(all_gemsets).should == true
+  end
+  it ".all_gemsets_but_global? should return 'false' when a gem exists in all gemsets and the global one" do 
+    gce = GemCov::GemCoverageEntry.new('aGem')
+    versions = ['1.0.0']
+    all_gemsets = ['another_gemset','my_gemset','global']
+    all_gemsets.collect {|gs| gce.add_gemset_versions(gs,versions) }
+    gce.in_all_gemsets_but_global?(all_gemsets).should == false
+  end
+  it ".all_gemsets_but_global? should return 'false' when a gem does not exist in all gemsets" do 
+    gce = GemCov::GemCoverageEntry.new('aGem')
+    versions = ['1.0.0']
+    my_name = 'mygemset'
+    all_gemsets = ['agemset','anothergemset','mygemset']
+    gce.add_gemset_versions(my_name,versions)
+    gce.in_all_gemsets_but_global?(all_gemsets).should == false
+  end
+  it ".all_gemsets_but_global? should return 'false' when a gem exists only in 'global'" do 
+    gce = GemCov::GemCoverageEntry.new('aGem')
+    versions = ['1.0.0']
+    my_name = 'global'
+    all_gemsets = ['global']
+    gce.add_gemset_versions(my_name,versions)
     gce.in_all_gemsets_but_global?(all_gemsets).should == false
   end
 end
