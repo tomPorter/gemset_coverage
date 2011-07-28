1f79a58ba898480205954d7402ba4f029e3cb93d
Tom Porter <thomas.porter@acm.org>
2011-07-28 13:57:36 -0400

    Further rspec cleanup

--- spec/gemhash_spec.rb
+++ spec/gemhash_spec.rb
@@ -1,7 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 module GemCov
-    describe GemHash  do
-      [:update!, :all_gems, :common_gems, :desired_gems, :default_gems].each do |method_to_check|
+  describe GemHash  do
+    methods_to_check = [:update!, :all_gems, :common_gems, :desired_gems, :default_gems]
+    methods_to_check.each do |method_to_check|
       it "should respond to #{method_to_check}" do
         subject.should respond_to method_to_check
       end
