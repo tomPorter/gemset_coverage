6392cb411bf7e94c777d990b7bd238c82780dbc0
Tom Porter <thomas.porter@acm.org>
2011-07-28 13:53:04 -0400

    Refactored GemHash tests

--- spec/gemhash_spec.rb
+++ spec/gemhash_spec.rb
@@ -1,20 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 module GemCov
-  describe GemHash  do
-    it "should respond to .update!" do
-      subject.should respond_to :update! 
-    end
-    it "should respond to .all_gems" do
-      subject.should respond_to :all_gems 
-    end
-    it "should respond to .common_gems" do
-      subject.should respond_to :common_gems
-    end
-    it "should respond to .desired_gems" do
-      subject.should respond_to :desired_gems
-    end
-    it "should respond to .default_gems" do
-      subject.should respond_to :default_gems
+    describe GemHash  do
+      [:update!, :all_gems, :common_gems, :desired_gems, :default_gems].each do |method_to_check|
+      it "should respond to #{method_to_check}" do
+        subject.should respond_to method_to_check
+      end
     end
   end
 end
